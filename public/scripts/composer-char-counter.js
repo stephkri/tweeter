7708e25b0ea1cc05ddb068f6dd8002826a1c82c2
stephkri <stephane.krims@hotmail.ca>
2021-09-11 03:27:19 +0000

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#tweet-text").keyup(function () {
+    let val = $(this).val();
+    let valLen = val.length
+    let count = 140 - valLen
+    const counter = $(this).parent()['0']['2'];
+    counter.innerText = count;
+    const counterObj = $(counter);
+    if (count >= 0) {
+      counterObj.css("color", "#545149");
+    }
+    if (count < 0) {
+      counterObj.css("color", "red");
+    }
+  });
+
+});
