bfd7fb9c24f0c74e041ffb9ed0e3444fa83c728b
stephkri <stephane.krims@hotmail.ca>
2021-09-15 03:56:21 +0000

    Session with Eamonn on adding tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+    <article class="tweet">
+    <header>
+      <div class="header-left">
+        <img src="${tweet.user.avatars}">
+        <h4 class="username">${tweet.user.name}</h4>
+      </div>
+      <div class="header-right">
+        ${tweet.user.handle}
+      </div>
+    </header>
+    <div class="tweet-proper">
+      <p>${tweet.content.text}</p>
+    </div>
+    <footer>
+      <div><p>${tweet['created_at']}</p></div>
+      <div class="icons">
+        <i class="fas fapad fa-flag"></i>
+        <i class="fas fapad fa-retweet"></i>
+        <i class="fas fapad fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+    `);
+    return $tweet;
+  };
+  
+  // Test/driver code
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
+  };
+  
+  // // Test / driver code (temporary)
+  renderTweets(data);
+
+  $(this).submit(function(event) {
+    console.log(event);
+    const tweetText = document.getElementById('tweet-text');
+    const rawText = tweetText.value;
+    event.preventDefault();
+    $.post("/tweets",
+    {
+      text: rawText
+    },
+    function(data, status){
+      alert("Data: " + data + "\nStatus: " + status);
+      $.get('/tweets', function(data, status) {
+        renderTweets(data);
+      });
+    });
+  });
+
+  // console.log(data);
+  // const dataString = JSON.stringify(data);
+  // const serialData = $(dataString).serialize();
+  // console.log('serial data:', serialData);
+
+});
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -8,6 +8,7 @@ const tweetsRoutes  = express.Router();
 module.exports = function(DataHelpers) {
 
   tweetsRoutes.get("/", function(req, res) {
+    console.log('here');
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -18,6 +19,8 @@ module.exports = function(DataHelpers) {
   });
 
   tweetsRoutes.post("/", function(req, res) {
+    console.log('post');
+    console.log('Req body in tweetsRoutes.post: ', req.body);
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
