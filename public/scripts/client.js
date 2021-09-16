385b438ff32d64373a18735c6a53fb4da01d6abb
stephkri <stephane.krims@hotmail.ca>
2021-09-16 03:50:36 +0000

    Delete commented out driver code in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,41 +32,12 @@ $(document).ready(function() {
     `);
     return $tweet;
   };
-  
-  // Test/driver code
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
 
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       $('#tweets-container').append(createTweetElement(tweet));
     }
   };
-  
-  // // Test / driver code (temporary)
-  //renderTweets(data);
 
   const loadTweets = function() {
     $.get('/tweets', function(data) {
@@ -90,10 +61,5 @@ $(document).ready(function() {
       loadTweets();
     });
   });
-
-  // console.log(data);
-  // const dataString = JSON.stringify(data);
-  // const serialData = $(dataString).serialize();
-  // console.log('serial data:', serialData);
 
 });
